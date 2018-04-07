b2805050481cbf92a318fda354d64d449617e7cb
liushuiping <1062576657@qq.com>
2018-04-07 23:28:59 +0800

    修改备注

--- app/src/main/java/shui/liu/com/mysend/MainActivity.java
+++ app/src/main/java/shui/liu/com/mysend/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //修改分支合并
+
     }
 
     private void getName(){
