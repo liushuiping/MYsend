385c6f466a6f337ed69fcf071c56129fc1e6e969
liushuiping <1062576657@qq.com>
2018-04-07 23:17:59 +0800

    修改备注

--- app/src/main/java/shui/liu/com/mysend/MainActivity.java
+++ app/src/main/java/shui/liu/com/mysend/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //修改了代码
     }
 
     private void getName(){
